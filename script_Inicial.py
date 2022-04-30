403106aabad1823fdff6a16f246a7a740403adcb
VLourenco23 <43066795+VLourenco23@users.noreply.github.com>
2022-04-30 21:48:06 +0100

    Camera.listen funcional

--- script_Inicial.py
+++ script_Inicial.py
@@ -15,39 +15,39 @@ import carla
 #import random
 import time
 
+
 def main():
-    actorList =[]
+    actorList = []
     try:
 
         client = carla.Client('localhost', 2000)
         client.set_timeout(10.0)
         world = client.load_world('Town02')
-        #print(client.get_available_maps())
+        # print(client.get_available_maps())
 
         blueprintLibrary = world.get_blueprint_library()
-        #print(blueprintLibrary)
+        # print(blueprintLibrary)
         vehicle_bp = blueprintLibrary.filter('mustang')[0]
-        transform = carla.Transform(carla.Location(x=180, y=237, z=20),carla.Rotation(yaw=180))
-        vehicle = world.spawn_actor(vehicle_bp,transform)
+        transform = carla.Transform(carla.Location(
+            x=180, y=237, z=20), carla.Rotation(yaw=180))
+        vehicle = world.spawn_actor(vehicle_bp, transform)
         actorList.append(vehicle)
-    
-        camera_bp=blueprintLibrary.find('sensor.camera.rgb')
-        camera_bp.set_attribute('image_size_x','800')
-        camera_bp.set_attribute('image_size_y','600') 
-        camera_transform = carla.Transform(carla.Location(x=1.5, z=2))
-        camera = world.spawn_actor(camera_bp,camera_transform, attach_to=vehicle)
 
-        #TODO: Camera.listen function
-        #camera.listen( Lambda image:save_to_disk('output/%d064.png'%image.frame))
+        camera_bp = blueprintLibrary.find('sensor.camera.rgb')
+        camera_bp.set_attribute('image_size_x', '800')
+        camera_bp.set_attribute('image_size_y', '600')
+        camera_transform = carla.Transform(carla.Location(x=1.5, z=2))
+        camera = world.spawn_actor(
+            camera_bp, camera_transform, attach_to=vehicle)
+
+        camera.listen(lambda image: image.save_to_disk(
+            'output/%d064.png' % image.frame))
         time.sleep(20)
-    
+
     finally:
         print('Delete actorList')
-        #client.apply_batch([carla.command.DestroyActor(x) for x in actorList])    
-    
-    finally:
-        print('Delete actorList')
-        #client.apply_batch([carla.command.DestroyActor(x) for x in actorList])    
-           
-if __name__=='__main__':
+        #client.apply_batch([carla.command.DestroyActor(x) for x in actorList])
+
+
+if __name__ == '__main__':
     main()
